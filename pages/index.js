2063c90eaa5c4b0ed4db6a9afafbf491f830523d
javion <228806684@qq.com>
2017-05-07 17:43:01 +0800

    init project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by h9zhou on 2017/5/7.
+ */
+export default () => (
+	<div>
+		<h1>Welcome to next.js!</h1>
+		<style jsx>{`
+      h1 {
+        color: blue;
+      }
+    `}</style>
+	</div>
+)
